d391b4c08d0ffe6cb6107e40decbd292c51fa787
Joshua Hernandez <jedchernandez@gmail.com>
2021-05-28 19:20:28 -0400

    feat: Add localStorage and fetch functionality

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContainer = document.getElementById("bookmarks-container");
 
+let bookmarks = [];
+
 // Show Modal, Focus on Input
 const showModal = () => {
   modal.classList.add("show-modal");
@@ -38,6 +40,23 @@ const validate = (nameValue, urlValue) => {
   return true;
 };
 
+// Fetch Bookmarks
+const fetchBookmarks = () => {
+  // Get bookmarks from localStorage if available
+  if (localStorage.getItem("bookmarks")) {
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    // Create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: "",
+        url: "",
+      },
+    ];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+};
+
 // Handle Data from Form
 let storeBookmark = (e) => {
   e.preventDefault();
@@ -49,7 +68,19 @@ let storeBookmark = (e) => {
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
 };
 
 // General Event Listener
 bookmarkForm.addEventListener("submit", storeBookmark);
+
+// On Load, Fetch Bookmarks
+fetchBookmarks();
